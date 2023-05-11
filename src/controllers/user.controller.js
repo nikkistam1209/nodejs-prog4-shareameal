c8311a020161ca653660182d829291629c374872
Nikki <nikki.stam@hotmail.com>
2023-05-11 13:06:40 +0200

    worked on use cases and test cases

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,63 +5,136 @@ const pool = require('../util/mysql-db');
 const userController = {
 
   // UC-201 nieuwe user registreren
-  createUser:((req, res) => {
+  createUser:((req, res, next) => {
     logger.info('register user')
-
     const user = req.body;
-    logger.debug('user = ', user);
+    logger.debug('user = ', user)
 
-    const sqlStatement = 'INSERT INTO `user` SET ?';
-    pool.getConnection((err, conn) => {
-        if (err) {
-            logger.error(err.message);
-            next({ code: 500, message: 'Failed to connect to the database' });
-            return;
-        }
-        conn.query(sqlStatement, user, (err, result) => {
+    try {
+        // validation: fields must be valid and filled out
+                // ik weet niet of straat/stad/phonenumber/roles etc. verplichte velden zijn 
+        assert(typeof user.firstName === 'string' && user.firstName.trim().length > 0 , 'firstName must be a string')
+        assert(typeof user.lastName === 'string' && user.lastName.trim().length > 0 , 'lastName must be a string')
+        // email address validation
+        assert(typeof user.emailAdress === 'string', 'emailAdress must be a string')
+        assert(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(user.emailAdress), 'emailAdress is not valid')
+        // password validation
+        assert(typeof user.password === 'string', 'password must be a string')
+        assert(user.password.length >= 8, 'password must be at least 8 characters long')
+        assert(/[a-z]/i.test(user.password), 'password must contain at least one letter')
+        assert(/[0-9]/.test(user.password), 'password must contain at least one number')
+        assert(/[^a-z0-9]/i.test(user.password), 'password must contain at least one special character')
+
+        pool.query('SELECT * FROM `user` WHERE `emailAdress` = ?', [user.emailAdress], (err, result) => {
             if (err) {
                 logger.error(err.message);
-                next({ code: 500, message: 'Failed to insert user data into the database' });
+                res.status(500).json({ 
+                    status: 500, 
+                    message: 'Failed to search for emailAdress in the database' 
+                });
+                return;
+            }   
+            try {
+                assert(result.length === 0, 'emailAdress already exists in the database')
+                const sqlStatement = 'INSERT INTO `user` SET ?'
+                pool.getConnection((err, conn) => {
+                    if (err) {
+                        logger.error(err.message);
+                        res.status(500).json({ 
+                            status: 500, 
+                            message: 'Failed to connect to the database' 
+                        });
+                        return;
+                    }
+                    conn.query(sqlStatement, user, (err, result) => {
+                        if (err) {
+                            logger.error(err.message);
+                            res.status(500).json({ 
+                                status: 500, 
+                                message: 'Failed to insert user data into the database' 
+                            });
+                            return;
+                        }
+                        user.id = result.insertId;
+                        logger.info('User registered successfully with ID:', result.insertId)
+                        res.status(201).json({ 
+                            status: 201, 
+                            message: 'Successfully registered user', 
+                            data: user 
+                        });
+                    })
+                    pool.releaseConnection(conn);
+                });
+            } catch (err) {
+                logger.warn(err.message)
+                res.status(403).json({
+                    status: 403,
+                    message: err.message // this is the assert message: the user/email already exists
+                });
                 return;
             }
-            logger.info('User registered successfully with ID:', result.insertId);
-            user.id = result.insertId;
-            res.status(201).json({ status: 201, message: 'Successfully registered user', data: user });
         });
-        pool.releaseConnection(conn);
-    });
-  
+    } catch (err) {
+        logger.warn(err.message)
+        res.status(400).json({
+            status: 400,
+            message: err.message // this is the assert message
+        });
+        return;
+    }
   }),
+
+
+
+
+
   
   // UC-202 opvragen alle users
   getAllUsers:(req, res, next) => {
     logger.info('get all users')
 
-    let sqlStatement = 'SELECT * FROM `user`';
+    const queryField = Object.entries(req.query)
+    // logger.info(`Queryfield 1 ${queryField[0][0]} = ${queryField[0][1]}`)
+    // logger.info(`Queryfield 2 ${queryField[1][0]} = ${queryField[1][1]}`)
+
+    //let sqlStatement = '';
+
+    // if (queryField.length == 2) {
+    //     sqlStatement = 'SELECT * FROM `user` WHERE '
+    // } else if (queryField.length = 1) {
+    //     sqlStatement = 'SELECT * FROM `user` WHERE  AND '
+    // } else {
+    //     sqlStatement = 'SELECT * FROM `user`'
+    // }
+
+    let sqlStatement = 'SELECT * FROM `user`'
 
     pool.getConnection(function (err, conn) {
         if (err) {
-          console.log('error', err);
-          next('error: ' + err.message);
+          logger.error(err.message)
+          next({
+            status: 500,
+            message: 'Failed to connect to the database' 
+          })
         }
         if (conn) {
           conn.query(sqlStatement, function (err, results, fields) {
             if (err) {
-              logger.err(err.message);
+              logger.err(err.message)
               next({
-                code: 409,
-                message: err.message
-              });
+                status: 500,
+                message: 'Failed to retrieve user data'
+              })
             }
             if (results) {
               logger.info('Found', results.length, 'results');
               res.status(200).json({
-                statusCode: 200,
+                status: 200,
                 message: 'User data endpoint',
                 data: results
-              });
+              })
             }
-          });
+          })
           pool.releaseConnection(conn);
         }
     });
@@ -70,19 +143,25 @@ const userController = {
 
   // UC-203 opvragen gebruikersprofiel
   getUserProfile:((req, res) => {
-    res.send('This function has not been implemented')
+    res.status(501).json({ 
+        status: 501, 
+        message: 'This function has not been implemented'
+    });
   })
   ,
 
 
   // UC-204 opvragen gegevens van een user
-  getUserById:( (req, res) => {
+  getUserById:( (req, res, next) => {
     logger.info('get user by id');
 
     const id = req.params.userId;
 
     if (!/^\d+$/.test(id)) {
-        res.status(401).json({ status: 401, error: 'Invalid token' });
+        res.status(401).json({ 
+            status: 401, 
+            message: 'Invalid token' 
+        });
         return;
     }
 
@@ -91,26 +170,49 @@ const userController = {
     pool.getConnection((err, conn) => {
         if (err) {
             logger.error(err.message);
-            next({ code: 500, message: 'Failed to connect to the database' });
+            next({ 
+                status: 500, 
+                message: 'Failed to connect to the database' 
+            });
             return;
         }
         conn.query(sqlStatement, id, (err, result) => {
             if (err) {
                 logger.error(err.message);
-                next({ code: 500, message: 'Failed to retrieve user data from the database' });
+                next({ 
+                    status: 500, 
+                    message: 'Failed to retrieve user data from the database' 
+                });
                 return;
             }
             if (result.length === 0) {
-                res.status(404).json({ status: 404, error: 'Unable to find user' });
+                res.status(404).json({ 
+                    status: 404, 
+                    message: 'Unable to find user' 
+                });
                 return;
             }
             const user = result[0];
             logger.info('User found:', user);
-            res.status(200).json({ status: 200, message: 'User data', data: user });
+            res.status(200).json({ 
+                status: 200, 
+                message: 'User data', 
+                data: user 
+            });
         });
         pool.releaseConnection(conn);
     });
   }),
+
+
+
+
+
+
+
+
+
+
   
   // UC-205 wijzigen gegevens van een user
   updateUser: (req, res, next) => {
@@ -124,58 +226,84 @@ const userController = {
     pool.getConnection((err, conn) => {
         if (err) {
             logger.error(err.message);
-            next({ code: 500, message: 'Failed to connect to the database' });
+            next({ 
+                status: 500, 
+                message: 'Failed to connect to the database' 
+            });
             return;
         }
         conn.query(sqlStatement, [updatedUser, id], (err, result) => {
             if (err) {
                 logger.error(err.message);
-                next({ code: 500, message: 'Failed to update user data in the database' });
+                next({ 
+                    status: 500, 
+                    message: 'Failed to update user data in the database' 
+                });
                 return;
             }
             if (result.affectedRows === 0) {
                 logger.warn('No user found with ID:', id);
-                res.status(404).json({ status: 404, message: `User with ID ${id} not found` });
+                res.status(404).json({ 
+                    status: 404, 
+                    message: `User with ID ${id} not found` 
+                });
                 return;
             }
             logger.info('User updated successfully with ID:', id);
-            res.status(200).json({ status: 200, message: 'Successfully updated user', data: { id, ...updatedUser } });
+            res.status(200).json({ 
+                status: 200, 
+                message: `Successfully updated user with ID ${id}`, 
+                data: { id, ...updatedUser } 
+            });
         });
         pool.releaseConnection(conn);
     });
-
-
-
-  }
+  },
   
-  ,
-  
+
+
+
+
+
+
+
+
   // UC-206 verwijderen van een user
-  deleteUser:(req, res) => {
+  deleteUser:(req, res, next) => {
 
     const userId = parseInt(req.params.userId);
     const sqlStatement = `DELETE FROM \`user\` WHERE id=${userId}`;
   
     pool.getConnection(function (err, conn) {
       if (err) {
-        console.log('error', err);
-        next('error: ' + err.message);
+        logger.error(err.message);
+            next({ 
+                status: 500, 
+                message: 'Failed to connect to the database' 
+            });
+            return;
       }
       if (conn) {
         conn.query(sqlStatement, function (err, results, fields) {
           if (err) {
-            logger.err(err.message);
+            logger.error(err.message);
             next({
-              code: 409,
-              message: err.message
+              status: 500,
+              message: 'Failed to delete user data from the database' 
             });
           }
           if (results.affectedRows === 0) {
             logger.info(`Unable to find user with ID ${userId}`);
-            res.status(404).json({ message: `Unable to find user with ID ${userId}` });
+            res.status(404).json({ 
+                status: 404,
+                message: `Unable to find user with ID ${userId}` 
+            });
           } else {
             logger.info(`Successfully deleted user with ID ${userId}`);
-            res.status(200).json({ message: `Succesfully deleted user with ID ${userId}` });
+            res.status(200).json({ 
+                status: 200,
+                message: `Successfully deleted user with ID ${userId}` 
+            });
           }
         });
         pool.releaseConnection(conn);
--- test/integration/users.test.js
+++ test/integration/users.test.js
@@ -1,38 +1,170 @@
 const chai = require('chai')
 const chaiHttp = require('chai-http')
 const server = require('../../index')
+const logger = require('../../src/util/utils').logger
+const assert = require('assert')
 chai.should()
 chai.use(chaiHttp)
 
-const assert = require('assert')
+
 
 describe('UC-201', function() {
-	it('TC-201-5 - User registered succesfully', (done) => {
+      it('TC-201-1 - Required field missing', (done) => {
 
-        const testUser = {
-            firstName: 'Kees', 
-            lastName: 'Jansen', 
-            emailAddress: 'kees@avans.nl'
-        }
+            const testUser = {
+                  firstName: '', 
+                  lastName: 'Jansen', 
+                  emailAdress: 'kees@avans.nl',
+                  password: 'testpassword1!',
+                  phoneNumber: '0612345678',
+                  roles: '',
+                  street: 'teststreet', 
+                  city: 'testcity'
+            }
+            chai.request(server)
+            .post('/api/user')
+            .send(testUser)
+            .end((err,res)=>{
+                  assert(err === null);
+      
+                  res.body.should.be.an('object')
+                  let {status, message} = res.body;
+      
+                  status.should.equal(400)
+                  message.should.equal('firstName must be a string')
+
+                  done()
+            })
+      });
+
+      it('TC-201-2 - Invalid email', (done) => {
+
+            const testUser = {
+                firstName: 'Kees', 
+                lastName: 'Jansen', 
+                emailAdress: 'notavalidemail',
+                password: 'testpassword1!',
+                phoneNumber: '0612345678',
+                roles: '',
+                street: 'teststreet', 
+                city: 'testcity'
+            }
+            chai.request(server)
+            .post('/api/user')
+            .send(testUser)
+            .end((err,res)=>{
+                  assert(err === null);
+
+                  res.body.should.be.an('object')
+                  let {status, message} = res.body;
+
+                  status.should.equal(400)
+                  message.should.equal('emailAdress is not valid')
+
+                  done()
+            })
+      });
+
+      it('TC-201-3 - Invalid password', (done) => {
+
+            const testUser = {
+                firstName: 'Kees', 
+                lastName: 'Jansen', 
+                emailAdress: 'kees@avans.nl',
+                password: 'notavalidpassword',
+                phoneNumber: '0612345678',
+                roles: '',
+                street: 'teststreet', 
+                city: 'testcity'
+            }
+            chai.request(server)
+            .post('/api/user')
+            .send(testUser)
+            .end((err,res)=>{
+                  assert(err === null);
+
+                  res.body.should.be.an('object')
+                  let {status, message} = res.body;
+
+                  status.should.equal(400)
+                  message.should.equal('password must contain at least one number')
+
+                  done()
+            })
+      });
+
+      it('TC-201-4 - Email already in use', (done) => {
+
+            const testUser = {
+                firstName: 'Kees', 
+                lastName: 'Jansen', 
+                emailAdress: 'm.vandullemen@server.nl',
+                password: 'testpassword1!',
+                phoneNumber: '0612345678',
+                roles: '',
+                street: 'teststreet', 
+                city: 'testcity'
+            }
+            chai.request(server)
+            .post('/api/user')
+            .send(testUser)
+            .end((err,res)=>{
+                  assert(err === null);
+
+                  res.body.should.be.an('object')
+                  let {status, message} = res.body;
+
+                  status.should.equal(403)
+                  message.should.equal('emailAdress already exists in the database')
+
+                  done()
+            })
+      });
+
+      // zorgen dat kees er niet in staat 
+	it('TC-201-5 - User registered successfully', (done) => {
+
+            const testUser = {
+                  firstName: 'Kees', 
+                  lastName: 'Jansen', 
+                  emailAdress: 'kees@avans.nl',
+                  password: 'testpassword1!',
+                  phoneNumber: '0612345678',
+                  roles: '',
+                  street: 'teststreet', 
+                  city: 'testcity'
+            }
 		chai.request(server)
-		.post('/api/user/register')
-        .send(testUser)
+		.post('/api/user')
+            .send(testUser)
 		.end((err,res)=>{
-            assert(err === null);
+                  assert(err === null);
 
-            res.body.should.be.an('object')
-            let {status, message, data} = res.body;
+                  res.body.should.be.an('object')
+                  let {status, message, data} = res.body;
 
-            status.should.equal(201)
-            message.should.equal('Succesfully registered user')
-            data.should.be.an('object')
-            data.id.should.equal(3);
-            data.firstName.should.equal('Kees');
-            data.lastName.should.equal('Jansen');
-            done()
+                  status.should.equal(201)
+                  message.should.equal('Successfully registered user')
+                  data.should.be.an('object')
+                  //data.id.should.equal(6); index problemen zorgen ervoor dat het id niet altijd 6 zal zijn
+                  data.firstName.should.equal('Kees');
+                  data.lastName.should.equal('Jansen');
+                  data.emailAdress.should.equal('kees@avans.nl');
+                  data.password.should.equal('testpassword1!');
+                  data.phoneNumber.should.equal('0612345678');
+                  data.street.should.equal('teststreet');
+                  data.city.should.equal('testcity');
+
+                  done()
 		})
 	});
 })
+
+
+
+
+
+
 
 describe('UC-202', function() {
 	it('TC-202-1 - Return all user data', (done) => {
@@ -41,21 +173,23 @@ describe('UC-202', function() {
 		.end((err,res)=>{
             assert(err === null);
 
-            res.body.should.be.an('object');
-            let {status, message, data} = res.body;
+                  res.body.should.be.an('object');
+                  let {status, message, data} = res.body;
 
-            status.should.equal(200);
-            message.should.equal('User data');
-            data.should.be.an('array');
+                  status.should.equal(200);
+                  message.should.equal('User data endpoint');
+                  data.should.be.an('array');
+                  data.length.should.be.above(1);
 
-            data.forEach(user => {
-                user.should.have.property('id');
-                user.should.have.property('firstName');
-                user.should.have.property('lastName');
-                user.should.have.property('emailAddress');
-              });
+                  data.forEach(user => {
+                  user.should.have.property('id');
+                  user.should.have.property('firstName');
+                  user.should.have.property('lastName');
+                  user.should.have.property('emailAdress');
+                  user.should.have.property('password');
+                  });
 
-            done();
+                  done();
 		})
 	});
 })
@@ -73,30 +207,134 @@ describe('UC-203', function() {
 })
 
 describe('UC-204', function() {
+      it.skip('TC-204-1 - Invalid token', (done) => {
+		chai.request(server)
+		.get('/api/user/invalid')
+		.end((err,res)=>{
+            assert(err === null);
+
+                  res.body.should.be.an('object')
+                  let {status, message} = res.body;
+
+                  status.should.equal(401)
+                  message.should.equal('Invalid token')
+
+                  done()
+		})
+	});
+      it('TC-204-2 - User ID does not exist', (done) => {
+		chai.request(server)
+		.get('/api/user/200')
+		.end((err,res)=>{
+            assert(err === null);
+
+                  res.body.should.be.an('object')
+                  let {status, message} = res.body;
+
+                  status.should.equal(404)
+                  message.should.equal('Unable to find user')
+
+                  done()
+		})
+	});
 	it('TC-204-3 - User ID exists', (done) => {
 		chai.request(server)
 		.get('/api/user/1')
 		.end((err,res)=>{
+                  assert(err === null);
+
+                  res.body.should.be.an('object')
+                  let {status, message, data} = res.body;
+
+                  status.should.equal(200)
+                  message.should.equal('User data')
+                  data.should.be.an('object')
+                  data.id.should.equal(1);
+                  data.firstName.should.equal('Mariëtte');
+                  data.lastName.should.equal('van den Dullemen');
+                  data.emailAdress.should.equal('m.vandullemen@server.nl')
+                  data.password.should.equal('secret')
+
+                  done()
+		})
+	});
+})
+
+describe('UC-205', function() {
+
+      // tc 205-1
+
+      it('TC-205-4 - User not found', (done) => {
+            const id = 200
+            const testUser = {
+                  firstName: 'Klaas', 
+                  lastName: 'Jansen', 
+                  emailAdress: 'klaas@avans.nl',
+            }
+		chai.request(server)
+		.put(`/api/user/${id}`)
+            .send(testUser)
+		.end((err,res)=>{
             assert(err === null);
 
-            res.body.should.be.an('object')
-            let {status, message, data} = res.body;
+            res.body.should.be.an('object');
+            let {status, message} = res.body;
 
-            status.should.equal(200)
-            message.should.equal('User data')
-            data.should.be.an('object')
-            data.id.should.equal(1);
-            data.firstName.should.equal('Nikki');
-            data.lastName.should.equal('Stam');
-            data.emailAddress.should.equal('nikki.stam@hotmail.com')
-            done()
+            status.should.equal(404);
+            message.should.equal(`User with ID ${id} not found`);
+
+            done();
+		})
+	});
+
+
+      // zorgen dat het id van een user is die geupdate kan worden
+	it('TC-205-6 - User successfully updated', (done) => {
+            const id = 63
+            const testUser = {
+                  firstName: 'Klaas', 
+                  lastName: 'Jansen', 
+                  emailAdress: 'klaas@avans.nl',
+            }
+		chai.request(server)
+		.put(`/api/user/${id}`)
+            .send(testUser)
+		.end((err,res)=>{
+            assert(err === null);
+
+            res.body.should.be.an('object');
+            let {status, message} = res.body;
+
+            status.should.equal(200);
+            message.should.equal(`Successfully updated user with ID ${id}`);
+
+            done();
 		})
 	});
 })
 
 describe('UC-206', function() {
-	it('TC-206-4 - User succesfully deleted', (done) => {
-        const id = 1
+      it('TC-206-1 - User not found', (done) => {
+            const id = 200
+		chai.request(server)
+		.delete(`/api/user/${id}`)
+		.end((err,res)=>{
+            assert(err === null);
+
+            res.body.should.be.an('object');
+            let {status, message} = res.body;
+
+            status.should.equal(404);
+            message.should.equal(`Unable to find user with ID ${id}`);
+
+            done();
+		})
+	});
+
+
+      // zorgen dat het id van een user is die verwijderd kan worden
+	it('TC-206-4 - User successfully deleted', (done) => {
+            const id = 63
 		chai.request(server)
 		.delete(`/api/user/${id}`)
 		.end((err,res)=>{
@@ -106,7 +344,7 @@ describe('UC-206', function() {
             let {status, message} = res.body;
 
             status.should.equal(200);
-            message.should.equal(`Succesfully deleted user with ID ${id}`);
+            message.should.equal(`Successfully deleted user with ID ${id}`);
 
             done();
 		})
